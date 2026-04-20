8646d64caf8044c72ff2cabc18768056f4c203b3
ines <ines.bourghida@etudiant-fsegt.utm.tn>
2026-04-20 22:33:45 +0100

    interaction

---
+++ InteractionService.java
@@ -0,0 +1,31 @@
+public class InteractionService {
+
+    // CF-10 : Commenter
+    static void commenter(int postId, String content) {
+        if (!Session.isLoggedIn())
+            throw new IllegalStateException("Connexion requise.");
+        for (Post p : PostService.posts) {
+            if (p.id == postId) {
+                p.comments.add(Session.currentUser.username + " : " + content);
+                System.out.println("Commentaire ajouté sur Post#" + postId);
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Post introuvable.");
+    }
+
+    // CF-11 : Liker
+    static void liker(int postId) {
+        if (!Session.isLoggedIn())
+            throw new IllegalStateException("Connexion requise.");
+        for (Post p : PostService.posts) {
+            if (p.id == postId) {
+                p.likes++;
+                System.out.println(Session.currentUser.username
+                    + " a liké Post#" + postId + " (" + p.likes + " likes)");
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Post introuvable.");
+    }
+}
